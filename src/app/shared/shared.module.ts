fe4d699eb460c976c3274cac7b100ce492aaccc4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-17 22:38:02 +0530

    add nav component inside shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule
