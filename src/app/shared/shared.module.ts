47670c69ab59db89be945e674fbb6ad3f9614be0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-18 23:38:52 +0530

    add divider component inside Shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { DividerComponent } from './divider/divider.component';
 
 @NgModule({
-  declarations: [NavComponent, FooterComponent],
+  declarations: [NavComponent, FooterComponent, DividerComponent],
   imports: [CommonModule, SharedRoutingModule],
   exports: [NavComponent],
 })
