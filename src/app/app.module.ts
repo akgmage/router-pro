2d602ed87a7b14857dfe3af3b7ca4840b186972e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-18 23:03:57 +0530

    add contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { SharedModule } from './shared/shared.module';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
