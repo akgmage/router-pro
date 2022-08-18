2a3d16ee092728de78e765064f98c36d33a08027
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-18 23:23:56 +0530

    add projects component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavComponent } from './shared/nav/nav.component';
 import { SharedModule } from './shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent, ProjectsComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
